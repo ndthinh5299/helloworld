9781479ba254577e14277b1be7aaab7ac78a8319
NgDinhThinh <thinhnguyenuet99@gmail.com>
2018-09-21 14:50:53 +0700

    thay doi

--- main.cpp
+++ main.cpp
@@ -71,16 +71,7 @@ int main()
     return 0;
 }
 
-string ltrim(const string &str) {
-    string s(str);
 
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
 
 string rtrim(const string &str) {
     string s(str);
